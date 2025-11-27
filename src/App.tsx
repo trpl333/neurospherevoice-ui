289cc88f866d135acdacb1d71ed8021412a7fe5b
trpl333 <jpeterson@bidprep.com>
2025-11-27 09:38:34 -0800

    Change BrowserRouter basename to '/'

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import i18n from "./i18n";
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={__BASE_PATH__}>
+      <BrowserRouter basename="/">
         <AppRoutes />
       </BrowserRouter>
     </I18nextProvider>
